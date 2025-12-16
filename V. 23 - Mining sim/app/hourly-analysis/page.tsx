51624f94b54af25f7920585062f1d8b2182e7140
T-Julsgaard <T.Julsgaard@proton.me>
2025-12-16 12:48:42 +0100

    Delete V. 23 - Mining sim directory

--- V. 23 - Mining sim/app/hourly-analysis/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function HourlyAnalysisPage() {
-  // Redirect to the main page since hourly analysis is now the landing page
-  redirect("/")
-}
-
